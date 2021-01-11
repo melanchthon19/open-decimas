43d5713be86dfd52afa98f04221868f98037bd3f
Daniel Mora Melanchthon <danielmora@Daniels-MacBook-Pro.local>
2021-01-10 23:55:54 -0300

    sum up syllables

--- fonetizador.py
+++ fonetizador.py
@@ -5,16 +5,18 @@ import pandas as pd
 import re
 import sys
 
-vowels_strong = {'a':'F', 'á':'F', 'e':'F', 'é':'F', 'í':'F', 'o':'F', 'ó':'F', 'ú':'F'}
+vowels_strong = {'a':'F', 'e':'F', 'o':'F',}
 vowels_weak = {'i':'D', 'u':'D', 'ü':'D'}
+vowels_accented = {'á':'A', 'é':'A', 'í':'A', 'ó':'A', 'ú':'A'}
 consonants = {'m':'C', 'n':'C', 'ñ':'C',
               'p':'C', 't':'C', 'k':'C', 'b':'C', 'd':'C', 'g':'C',
               'C':'C',
               'f':'C', 's':'C', 'L':'C', 'x':'C',
               'l':'C', 'r':'C', 'R':'C'}
-vowels = list(vowels_strong.keys()) + list(vowels_weak.keys())
-alphabet = list(vowels_strong.keys()) + list(vowels_weak.keys()) + list(consonants.keys())
-phonemes_dict = {**vowels_strong, **vowels_weak, **consonants}
+
+vowels = list(vowels_strong.keys()) + list(vowels_weak.keys()) + list(vowels_accented.keys())
+alphabet = vowels + list(consonants.keys())
+phonemes_dict = {**vowels_strong, **vowels_weak, **vowels_accented, **consonants}
 
 char2phone = {
        1:{
@@ -54,9 +56,12 @@ class Phonetizer():
         self.text_phoneme = []
         self.text_structure = []
         self.text_syllables = []
+        self.last_words = {}
 
     def word2phonemes(self, word):
-        phonemes = word.lower()
+        # findall retrieves a list of characters only
+        phonemes = ''.join(re.findall('[^\W]*', word.lower()))
+
         for rule in self.char2phone.keys():  # certain rules must be applied first
             for char in self.char2phone[rule]:
                 if char in phonemes:
@@ -67,7 +72,7 @@ class Phonetizer():
 
     def phonemes2structure(self, phonemes):
         structure = [phonemes_dict[phone] for phone in phonemes if phone in alphabet]
-        print(phonemes, structure)
+
         return structure
 
     def syllables_per_word(self, structure):
@@ -80,7 +85,6 @@ class Phonetizer():
                 # add catch block to handle out-of-language words
                 break
         structure = ''.join(structure)
-        print(structure)
         number_syllables = structure.count('V')
 
         return number_syllables
@@ -88,6 +92,9 @@ class Phonetizer():
     def text2structure(self):
         if self.text_raw == None:
             raise AttributeError('pass a txt file to read first')
+
+        self.last_words = {i:'' for i in range(len(self.text_raw))}
+
         for sentence in range(len(self.text_raw)):
             sentence_structure = []
             sentence_phonemes = []
@@ -99,15 +106,47 @@ class Phonetizer():
 
                 structure = self.phonemes2structure(phonemes)
                 sentence_structure.append(structure)
+                #print(phonemes, structure)
+                if i == len(self.text_raw[sentence]) - 1:  # processing the last word
+                    last_word = self.metric_rule(phonemes, structure)
+                else:
+                    last_word = 0
 
                 number_syllables = self.syllables_per_word(structure[:])
-                sentence_syllables.append(number_syllables)
+                sentence_syllables.append(number_syllables + last_word)
 
             self.text_phoneme.append(sentence_phonemes)
             self.text_structure.append(sentence_structure)
             self.text_syllables.append(sentence_syllables)
 
         return
+
+    def acentuacion(self, phonemes, structure):
+        #if any(char in phonemes for char in vowels_accented):
+        if 'A' in structure:
+            pos = structure.index('A')
+            if (structure[-1] == 'A') or (structure[-2] == 'A' and phonemes[-1] in ['n','s']):
+                return 'aguda'
+            elif structure[pos:].count('D') + structure[pos:].count('F') >= 2:
+                return 'esdrujula'
+            else:
+                return 'grave'
+
+        else:
+            if phonemes[-1] in (vowels + ['n','s']):
+                return 'grave'
+            else:
+                return 'aguda'
+
+    def metric_rule(self, phonemes, structure):
+        accent = self.acentuacion(phonemes, structure)
+        print(phonemes, structure, accent)
+        if accent == 'aguda':
+            return +1
+        elif accent == 'grave':
+            return 0
+        else:  # accent == 'esdrujula':
+            return -1
 
     def read_txt(self, file):
         with open(file, 'r') as f:
@@ -141,3 +180,6 @@ phonetizer2 = Phonetizer(vowels, consonants, char2phone)
 phonetizer2.read_txt('decima1.txt')
 phonetizer2.text2structure()
 #phonetizer2.print_structure(3)
+phonetizer2 = Phonetizer(vowels, consonants, char2phone)
+phonetizer2.read_txt('decima2.txt')
+phonetizer2.text2structure()
